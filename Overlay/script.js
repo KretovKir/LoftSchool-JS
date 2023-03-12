cbb620c9c7da1820d02d0721106c08e2355e8aaa
Kirill Kretov <113347342+KretovKir@users.noreply.github.com>
2023-03-12 19:15:02 +0300

    use createModal

--- Overlay/script.js
+++ Overlay/script.js
@@ -1,44 +1,45 @@
+const openButton = document.querySelector("#openOverlay");
+const body = document.body;
+const successModal = createModal("Overlay test");
 
+openButton.addEventListener("click", function(event) {
+  body.appendChild(successModal);
+});
 
-function createModal(content){
-    const openBtn = document.querySelector('#openOverlay');
-    const body = document.body;
-    openBtn.addEventListener('click', function(event){
-        const overlay = document.createElement('div');
-        overlay.classList.add('overlay');
-        
-    
-        overlay.addEventListener('click', function(event){
-            if (event.target === overlay){
-                closeElement.click();
-            } 
-        })
-    
-        const containerElement = document.createElement('div');
-        containerElement.classList.add('modal-container');
-        
-    
-        const contentElement = document.createElement('div');
-        contentElement.classList.add('content');
-        contentElement.innerHTML=`${content}`;
-    
-       
-    
-        const closeElement = document.createElement('a');
-        closeElement.classList.add('close');
-        closeElement.textContent ='x';
-        
-    
-        closeElement.addEventListener('click', function(event){
-            event.preventDefault();
-            body.removeChild(overlay);
-        })
+function createModal(content) {
+  const overlayElement = document.createElement("div");
+  overlayElement.classList.add("overlay");
 
-        body.appendChild(overlay);
-        overlay.appendChild(containerElement);
-        containerElement.appendChild(contentElement);
-        containerElement.appendChild(closeElement);
-    })
+  overlayElement.addEventListener("click", function(event) {
+    if (!event.target.classList.contains("content")) {
+      closeElement.click();
+    }
+    // if (event.target === overlayElement) {
+    //   closeElement.click();
+    // }
+  });
+
+  const containerElement = document.createElement("div");
+  containerElement.classList.add("modal-container");
+
+  const contentElement = document.createElement("div");
+  contentElement.classList.add("content");
+
+  contentElement.innerHTML = content;
+
+  const closeElement = document.createElement("a");
+  closeElement.classList.add("close");
+  closeElement.textContent = "x";
+  closeElement.href = "#";
+
+  closeElement.addEventListener("click",function(event){
+    event.preventDefault();
+    body.removeChild(overlayElement);
+  });
+
+  overlayElement.appendChild(containerElement);
+  containerElement.appendChild(closeElement);
+  containerElement.appendChild(contentElement);
+
+  return overlayElement;
 }
-
-createModal('Lalalala')
